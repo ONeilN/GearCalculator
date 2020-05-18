c23451d93feb4da2de72a105d06a56e9e9a550eb
OneilN <aizatjedisg@gmail.com>
2020-05-19 00:51:32 +0300

    Added some methods

--- src/main/java/com/nugumanov/gearcalculator/controller/CalcController.java
+++ src/main/java/com/nugumanov/gearcalculator/controller/CalcController.java
@@ -1,6 +1,6 @@
 package com.nugumanov.gearcalculator.controller;
 
-import com.nugumanov.gearcalculator.domain.Gear;
+import com.nugumanov.gearcalculator.domain.GearDomain;
 import com.nugumanov.gearcalculator.repos.GearRepo;
 import com.nugumanov.gearcalculator.util.CalcUtil;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,6 +12,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import ru.itis.software.engineering.gear.library.GearLibrary;
 import ru.itis.software.engineering.gear.library.Module;
+import ru.itis.software.engineering.gear.library.gearExample.Gear;
 
 import java.util.Optional;
 
@@ -27,15 +28,33 @@ public class CalcController {
         return "calc";
     }
 
+    @RequestMapping(value = "/calc", method = RequestMethod.POST, params = "getDiameter")
+    public String getDiameter(
+            @ModelAttribute CalcUtil calcUtil,
+            @RequestParam(required = false, defaultValue = "") String diameter,
+            Model model
+    ) {
+        Optional<GearDomain> gearDomain = gearRepo.findById(calcUtil.getId1());
+
+        Gear gear = gearDomain.get().getGear();
+
+        diameter = Double.toString(gear.getDiameter());
+
+        model.addAttribute("calcUtil", calcUtil);
+        model.addAttribute("diameter", diameter);
+
+        return "calc";
+    }
+
     @RequestMapping(value = "/calc", method = RequestMethod.POST, params = "getGearRatio")
     public String getGearRatio(
             @ModelAttribute CalcUtil calcUtil,
             @RequestParam(required = false, defaultValue = "") String result,
             Model model
     ) {
-        Optional<Gear> gear1 = gearRepo.findById(calcUtil.getId1());
-        Optional<Gear> gear2 = gearRepo.findById(calcUtil.getId2());
-        double ratio = GearLibrary.GEAR_LIBRARY.getGearRatio(gear1.get().getTeeth(), gear2.get().getTeeth());
+        Optional<GearDomain> gear1 = gearRepo.findById(calcUtil.getId1());
+        Optional<GearDomain> gear2 = gearRepo.findById(calcUtil.getId2());
+        double ratio = GearLibrary.GEAR_LIBRARY.getGearRatio(gear1.get().getTeeth1(), gear2.get().getTeeth1());
         result = Double.toString(ratio);
 
         model.addAttribute("calcUtil", calcUtil);
@@ -49,14 +68,54 @@ public class CalcController {
             @RequestParam(required = false, defaultValue = "") String distance,
             Model model
     ) {
-        Optional<Gear> gear1 = gearRepo.findById(calcUtil.getId1());
-        Optional<Gear> gear2 = gearRepo.findById(calcUtil.getId2());
-        double ratio = GearLibrary.GEAR_LIBRARY.getGearCenterDistance(gear1.get().getTeeth(), gear2.get().getTeeth(), calcUtil.getTeethAngle(), Module.valueOf(gear1.get().getModule()));
+        Optional<GearDomain> gear1 = gearRepo.findById(calcUtil.getId1());
+        Optional<GearDomain> gear2 = gearRepo.findById(calcUtil.getId2());
+        double ratio = GearLibrary.GEAR_LIBRARY.getGearCenterDistance(gear1.get().getTeeth1(), gear2.get().getTeeth1(), calcUtil.getTeethAngle(), Module.valueOf(gear1.get().getModule()));
 
         distance = Double.toString(ratio);
 
         model.addAttribute("calcUtil", calcUtil);
         model.addAttribute("distance", distance);
+
+        return "calc";
+    }
+
+    @RequestMapping(value = "/calc", method = RequestMethod.POST, params = "getGearDividingDiameter")
+    public String getGearDividingDiameter(
+            @ModelAttribute CalcUtil calcUtil,
+            @RequestParam(required = false, defaultValue = "") String dividing,
+            Model model
+    ) {
+        Optional<GearDomain> gearDomain = gearRepo.findById(calcUtil.getId1());
+
+        double result = GearLibrary.GEAR_LIBRARY.getGearDividingDiameter(gearDomain.get().getTeeth1(), gearDomain.get().getAngle(), Module.valueOf(gearDomain.get().getModule()));
+
+        dividing = Double.toString(result);
+
+        model.addAttribute("calcUtil", calcUtil);
+        model.addAttribute("dividing", dividing);
+
+        return "calc";
+    }
+
+    @RequestMapping(value = "/calc", method = RequestMethod.POST, params = "getGearToothTopDiameter")
+    public String getGearToothTopDiameter(
+            @ModelAttribute CalcUtil calcUtil,
+            @RequestParam(required = false, defaultValue = "") String topdiameter,
+            Model model
+    ) {
+        Optional<GearDomain> gearDomain = gearRepo.findById(calcUtil.getId1());
+
+        Gear gear = gearDomain.get().getGear();
+        double deltaY = GearLibrary.GEAR_LIBRARY.getDeltaY(gearDomain.get().getTeeth1(), gearDomain.get().getTeeth2(), gearDomain.get().getAngle(), Module.valueOf(gearDomain.get().getModule()));
+
+        double result = GearLibrary.GEAR_LIBRARY.getGearToothTopDiameter(gear.getGearTeeth(), gear.getTeethAngle(), gear.getGearModule(), gearDomain.get().getX(), deltaY);
+        System.out.println(result);
+        topdiameter = Double.toString(result);
+
+        model.addAttribute("calcUtil", calcUtil);
+        model.addAttribute("topdiameter", topdiameter);
+
         return "calc";
     }
 }
--- src/main/java/com/nugumanov/gearcalculator/repos/GearRepo.java
+++ src/main/java/com/nugumanov/gearcalculator/repos/GearRepo.java
@@ -1,8 +1,8 @@
 package com.nugumanov.gearcalculator.repos;
 
-import com.nugumanov.gearcalculator.domain.Gear;
+import com.nugumanov.gearcalculator.domain.GearDomain;
 import org.springframework.data.repository.CrudRepository;
 
-public interface GearRepo extends CrudRepository<Gear, Integer> {
+public interface GearRepo extends CrudRepository<GearDomain, Integer> {
 
 }
