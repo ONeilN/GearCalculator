aaeb9ee8924c931bdfa07a00586ebb8f904dac27
OneilN <aizatjedisg@gmail.com>
2020-06-10 17:32:31 +0300

    Some ui changes

--- src/main/java/com/nugumanov/gearcalculator/controller/PulleyController.java
+++ src/main/java/com/nugumanov/gearcalculator/controller/PulleyController.java
@@ -1,14 +1,14 @@
 package com.nugumanov.gearcalculator.controller;
 
-import com.nugumanov.gearcalculator.domain.GearDomain;
 import com.nugumanov.gearcalculator.domain.PulleyDomain;
-import com.nugumanov.gearcalculator.repos.GearRepo;
 import com.nugumanov.gearcalculator.repos.PulleyRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class PulleyController {
